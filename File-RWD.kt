3581d384fc7dfb5bd75d151e31abc29ac63fb271
shangguan-chenxi <50815074+shangguan-chenxi@users.noreply.github.com>
2021-10-04 03:54:04 +1100

    Create File-RWD.kt

---
+++ File-RWD.kt
@@ -0,0 +1,66 @@
+    // 在build.gradle(Module) 中添加以下代码：buildFeatures { viewBinding true }
+    // 思考：怎样向文件里追加一行，读取/修改/删除指定一行
+    
+class MainActivity : AppCompatActivity() {
+    
+    private lateinit var ui : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        ui = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(ui.root)
+
+        // 文件参数
+        val fileName = "record.txt"
+        val file = File(filesDir, fileName) // 内部储存
+
+        /** 写入文件 */
+        ui.buttonWrite.setOnClickListener {
+            try {
+                var content = "0123456789ABCDEF"
+                if (!file.exists()) {
+                    file.createNewFile() //?????
+                }else{
+                    content = "ABCDEF0123456789"
+                }
+                val fos = FileOutputStream(file)
+                fos.write(content.toByteArray())
+                fos.close()
+                if (file.exists()) {
+                    AlertDialog.Builder(this).setTitle("提示").setMessage("已经写入 ${filesDir}").setPositiveButton("好", null).create().show()
+                }else{
+                    AlertDialog.Builder(this).setTitle("提示").setMessage("写入失败").setPositiveButton("好", null).create().show()
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+        /** 读取文件 */
+        ui.buttonRead.setOnClickListener {
+            try {
+                if (!file.exists()) {
+                    AlertDialog.Builder(this).setTitle("提示").setMessage("文件不存在").setPositiveButton("好", null).create().show()
+                }else {
+                    val fos = FileInputStream(file)
+                    val byte = ByteArray(1024)
+                    val len = fos.read(byte)
+                    val readContent = String(byte, 0, len)
+                    AlertDialog.Builder(this).setTitle("提示").setMessage("文件内容: $readContent").setPositiveButton("好", null).create().show()
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+        /** 删除文件 */
+        ui.buttonDelete.setOnClickListener {
+            if (!file.exists()) {
+                AlertDialog.Builder(this).setTitle("提示").setMessage("文件不存在，无法删除").setPositiveButton("好", null).create().show()
+            }else if (file.delete()){
+                AlertDialog.Builder(this).setTitle("提示").setMessage("文件成功删除").setPositiveButton("好", null).create().show()
+            }
+        }
+    }
+}
